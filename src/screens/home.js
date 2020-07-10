71ad0936ff77db25555da76e633e8cc00f0208fb
Jestin Abraham <jestintab@Jestins-MacBook-Pro.local>
2020-07-11 00:58:42 +0300

    ready for git

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,35 +1,122 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, TextInput, Icon} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
-Home = () => {
-  const navigation = useNavigation();
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <View style={styles.container}>
-      
-      <Button
-        title="User Login"
-        onPress={() =>
-          navigation.navigate('Map')
-        }
-      />
-    </View>
-  );
-};
+    this.state = {
+      username: '',
+      password: '',
+      alertmsg: ''
+    };
 
-export default Home;
+    this._authenticate=this._authenticate.bind(this);
+  }
+  _authenticate(){
+    //alert("loggin");
+    //console.log(this.state.username)
+   fetch('http://localhost:5000/drivers/login',{
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            username: this.state.username.toLowerCase(),
+            password: this.state.password
+            }),
+          })
+          .then((response) => response.json())
+          .then((res) =>{
+            console.log(res);
+            if(res.isAuth === 1){
+              console.log('login success');
+              this.props.navigation.navigate('Map',{
+                driver: res.driver
+              });
+            }else{
+             alert(res.alertmsg);
+            }
+               
+             })
+             .catch((error) => {
+              console.error(error);
+            });
+          
+
+   // this.props.navigation.navigate('Map');
+  }
+  render() {
+    
+    return (
+      <View style={{flex: 1, alignItems: 'center', paddingTop:100,backgroundColor: 'white'}}>
+        <Text> LOGIN HERE</Text>
+        <View style={styles.container}>
+          <TextInput
+            value={this.state.username}
+            onChangeText={username => this.setState({username})}
+            placeholder={'Username'}
+            style={styles.input}
+            underlineColorAndroid="rgba(131,157,182,.7)"
+            autoCapitalize="none"
+            placeholderTextColor="rgba(131,157,182,.7)"
+          />
+        </View>
+        <View style={styles.container}>
+          <TextInput
+            value={this.state.password}
+            onChangeText={password => this.setState({password})}
+            placeholder={'Password'}
+            secureTextEntry={true}
+            style={styles.input}
+            underlineColorAndroid="rgba(131,157,182,.7)"
+            autoCapitalize="none"
+            placeholderTextColor="rgba(131,157,182,.7)"
+          />
+        </View>
+        <Button style={styles.button}
+          title="Login"
+          width= "100%"
+          height= {50}
+          color= "#000"
+          alignItems= "center"
+          justifyContent= "center"
+   
+          onPress={ this._authenticate}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    //flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+
+    // backgroundColor: '#ecf0f1',
   },
   button: {
     paddingHorizontal: 20,
     paddingVertical: 10,
     marginVertical: 10,
     borderRadius: 5,
+    backgroundColor:"#3BD1DF"
   },
+
+  input: {
+    margin: 15,
+    width: 200,
+    height: 40,
+    borderBottomColor: "#d2dde8",
+    color: 'rgba(131,157,182,.7)',
+    borderBottomWidth: 1,
+    backgroundColor:'#e8f0fe',
+    fontSize: 13,
+  },
+  
 });
--- src/screens/map.js
+++ src/screens/map.js
@@ -1,22 +1,28 @@
 import React, {Component} from 'react';
-import {StyleSheet,
-    View,
-    Platform,
-    Dimensions,
-    SafeAreaView} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Platform,
+  Dimensions,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 import MapView, {
   Marker,
   AnimatedRegion,
   PROVIDER_GOOGLE,
+  Polyline,
 } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
-
+import haversine from 'haversine';
 
 const {width, height} = Dimensions.get('window');
 
 const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
+const LATITUDE = 25.3252981;
+const LONGITUDE = 51.5261059;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
@@ -24,8 +30,13 @@ export default class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      driver: props.route.params.driver,
+      trip_id: '',
       latitude: LATITUDE,
       longitude: LONGITUDE,
+      routeCoordinates: [],
+      distanceTravelled: 0,
+      prevLatLng: {},
       coordinate: new AnimatedRegion({
         latitude: LATITUDE,
         longitude: LONGITUDE,
@@ -33,6 +44,9 @@ export default class Map extends Component {
         longitudeDelta: 0,
       }),
     };
+
+    this._startdrive = this._startdrive.bind(this);
+    this._enddrive = this._enddrive.bind(this);
   }
 
   componentDidMount() {
@@ -40,41 +54,48 @@ export default class Map extends Component {
   }
 
   watchLocation = () => {
-    const { coordinate } = this.state;
+    const {routeCoordinates, distanceTravelled} = this.state;
+    const {coordinate} = this.state;
 
     this.watchID = Geolocation.watchPosition(
       position => {
-        const { latitude, longitude } = position.coords;
+        const {latitude, longitude} = position.coords;
 
         const newCoordinate = {
           latitude,
-          longitude
+          longitude,
         };
 
-        if (Platform.OS === "android") {
+        if (Platform.OS === 'android') {
           if (this.marker) {
             this.marker._component.animateMarkerToCoordinate(
               newCoordinate,
-              500 // 500 is the duration to animate the marker
+              500, // 500 is the duration to animate the marker
             );
           }
         } else {
           coordinate.timing(newCoordinate).start();
         }
-        console.log('lat:'+latitude+' '+'log:'+longitude);
+        //console.log('lat:' + latitude + ' ' + 'log:' + longitude);
 
         this.setState({
-          latitude,
-          longitude
+          latitude: latitude,
+          longitude: longitude,
+          routeCoordinates: routeCoordinates.concat([newCoordinate]),
+          distanceTravelled:
+            distanceTravelled + this.calcDistance(newCoordinate),
+          prevLatLng: newCoordinate,
         });
+        //console.log('km: '+this.state.distanceTravelled);
+        //console.log(this.state.latitude+' , '+ this.state.longitude );
       },
       error => console.log(error),
       {
         enableHighAccuracy: true,
         timeout: 20000,
         maximumAge: 1000,
-        distanceFilter: 10
-      }
+        distanceFilter: 1,
+      },
     );
   };
 
@@ -82,20 +103,84 @@ export default class Map extends Component {
     latitude: this.state.latitude,
     longitude: this.state.longitude,
     latitudeDelta: LATITUDE_DELTA,
-    longitudeDelta: LONGITUDE_DELTA
+    longitudeDelta: LONGITUDE_DELTA,
   });
+
+  calcDistance = newLatLng => {
+    const {prevLatLng} = this.state;
+    // console.log('hav '+prevLatLng+'  '+ newLatLng);
+    return haversine(prevLatLng, newLatLng, {unit: 'meter'}) || 0;
+  };
+
+  _startdrive() {
+    fetch('http://localhost:5000/drivers/trip/' + this.state.driver._id, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        trip: {
+          start_time: new Date(),
+          end_time: '',
+          kilometers: 0,
+          drivepath: [],
+        },
+        location: {lat: this.state.latitude, lng: this.state.longitude},
+      }),
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log(res);
+        const trip_id = res.driver.trips[res.driver.trips.length - 1]._id;
+        this.setState({
+          trip_id: trip_id,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  _enddrive() {
+    fetch('http://localhost:5000/drivers/endtrip/' + this.state.driver._id, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        trip_id: this.state.trip_id,
+        endtime: new Date(),
+        kilometers: this.state.distanceTravelled,
+        drivepaths: this.state.routeCoordinates,
+        location: {lat: this.state.latitude, lng: this.state.longitude},
+      }),
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log(res);
+        alert(res.msg);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
   render() {
     return (
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{flex: 1}}>
         <View style={styles.container}>
           <MapView
             style={styles.map}
             showUserLocation
             followUserLocation
             loadingEnabled
-            region={this.getMapRegion()}
-          >
+            region={this.getMapRegion()}>
+            <Polyline
+              coordinates={this.state.routeCoordinates}
+              strokeWidth={5}
+            />
             <Marker.Animated
               ref={marker => {
                 this.marker = marker;
@@ -103,6 +188,31 @@ export default class Map extends Component {
               coordinate={this.state.coordinate}
             />
           </MapView>
+          <View
+            style={{
+              position: 'absolute',
+              top: '0%',
+              alignSelf: 'flex-start',
+              backgroundColor: '#c6c6c6',
+            }}>
+            <Button title="Start New Drive" onPress={this._startdrive} />
+          </View>
+          <View
+            style={{
+              position: 'absolute',
+              top: '0%',
+              alignSelf: 'flex-end',
+              backgroundColor: '#c6c6c6',
+            }}>
+            <Button title="End Drive" onPress={this._enddrive} />
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={[styles.bubble, styles.button]}>
+              <Text style={styles.bottomBarContent}>
+                {parseFloat(this.state.distanceTravelled).toFixed(2)} km
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </SafeAreaView>
     );
@@ -110,12 +220,34 @@ export default class Map extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        ...StyleSheet.absoluteFillObject,
-        justifyContent: "flex-end",
-        alignItems: "center"
-      },
-      map: {
-        ...StyleSheet.absoluteFillObject
-      }
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 100,
+    backgroundColor: 'transparent',
+  },
 });
